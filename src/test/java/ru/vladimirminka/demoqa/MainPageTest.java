e72b9b2277d46aec9cbda5784aa52d1f488d47cc
Vladimir Minka <38921290+VladimirMinka@users.noreply.github.com>
2025-08-07 15:06:25 +0300

    Создана проверку, что кнопка "Visible After 5 Seconds" стала видимой.

---
+++ src/test/java/ru/vladimirminka/demoqa/MainPageTest.java
@@ -0,0 +1,44 @@
+package ru.vladimirminka.demoqa;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.get("https://demoqa.com/dynamic-properties");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void searchElements() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        WebElement btnAfter5Seconds = driver.findElement(By.id("visibleAfter"));
+        wait.until(ExpectedConditions.elementToBeClickable(btnAfter5Seconds));
+        assertTrue(btnAfter5Seconds.isEnabled(), "кнопка не активна");
+    }
+}
